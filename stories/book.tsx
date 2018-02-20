a3fac8098ae4062c82e25d17e6ac61cfd9b63776
Heejae Jang <heejae@ridi.com>
2018-02-20 11:56:39 +0900

    Improve Book component usage

--- stories/book.tsx
+++ stories/book.tsx
@@ -4,10 +4,21 @@ import { Book, dto2props } from '../components/'
 
 const book = require('./mocks/593000658.json')
 
-storiesOf('Book', module)
+storiesOf('Usage case', module)
   .add('basic', () => (
-    <Book
-      {...dto2props(book)}
-      thumbnailSize={110}
-    />
+    <Book {...dto2props(book)} />
+  ))
+  .add('custom', () => (
+    <Book {...dto2props(book)}>
+      {Root => <>
+        <Root.Thumbnail />
+        <Root.Metadata>
+          {Metadata => <>
+            <Metadata.starRate />
+            <Metadata.publisher />
+            <Metadata.authors />
+          </>}
+        </Root.Metadata>
+      </>}
+    </Book>
   ))
