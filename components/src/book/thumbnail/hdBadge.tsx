95021f11945d8ba50c589ba2f03774588e6021a9
Heejae Jang <heejae@ridi.com>
2018-02-21 15:04:26 +0900

    Fix: HdBadge declaration file transpile error

--- components/src/book/thumbnail/hdBadge.tsx
+++ components/src/book/thumbnail/hdBadge.tsx
@@ -4,10 +4,8 @@ export interface HDBadgeProps {
   isComicHD: boolean
 }
 
-const HDBadge: React.SFC<HDBadgeProps> = (props) => (props.isComicHD && 
+export const HDBadge: React.SFC<HDBadgeProps> = (props) => (props.isComicHD &&
   <div className="RSGBookThumbnail_HDBadge">
     <p className="HDBadge_Label">고화질</p>
   </div>
 )
-
-export { HDBadge }
