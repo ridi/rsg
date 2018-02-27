05c724db7e7190b2d4f9a0e13a055111be2b6ab1
nnyamm <nnyamm@ridi.com>
2018-02-27 14:51:03 +0900

    getCircleBadge refac  - 렌더링 조건로직을 좀더 명료하게 수정

--- components/src/book/dto/getCircleBadge.ts
+++ components/src/book/dto/getCircleBadge.ts
@@ -2,59 +2,53 @@ import { BookDto } from './types'
 import { CircleBadgeProps, CircleBadgeType } from '../thumbnail/children/circleBadge'
 
 export function getCircleBadge (dto: BookDto): CircleBadgeProps {
+  const isSeries = !!(dto.series && dto.series.property)
+  
+  // 대여
   const { genre } = dto.categories[0]
   const { isRental } = dto.property
+  const isRentalBadgeEnableGenre = ['general', 'romance'].includes(genre)
 
-  const isGeneral = ['general', 'romance'].includes(genre)
-  const isSeries = !!(dto.series && dto.series.property)
-  const isSetbook = !!dto.setbook
+  // 무료책
+  const MAX_FREE_BOOK_COUNT_FOR_DISCOUNT_RENDERING = 4;
+  const freeBookCount = isSeries ? dto.series.property.freeBookCount : 0;
+  const freeBookUnit = isSeries ? dto.series.property.unit : '';
+  const hasFreeBook = freeBookCount > 0
 
-  if (isGeneral && isRental) {
-    return { type: CircleBadgeType.Rental }
-  } else if (genre === 'general' || (isSeries && isGeneral) || isSetbook) {
+  // 할인율
+  const MIN_DISCOUNT_RATE = 10
+  const discountRate = (() => {
     const { buy, rent } = dto.priceInfo
+    // 100% 할인인 경우 무료도서 이기 때문에 할인율에서 제외
+    const buyDiscount = buy.discountPercentage === 100 ? 0 : buy.discountPercentage
+    const rentDiscount = rent.discountPercentage === 100 ? 0 : rent.discountPercentage
 
-    let discountRate = 0
-    let rentalDiscountRate = 0
+    let maxDiscountRate = Math.max(buyDiscount, rentDiscount)
 
-    if (buy) {
-      discountRate = buy.discountPercentage;
+    if (isSeries) {
+      const { buy: seriesBuy, rent: seriesRent } = dto.series.priceInfo
+      const seriesBuyDiscount = seriesBuy.discountPercentage === 100 ? 0 : seriesBuy.discountPercentage
+      const seriesRentDiscount = seriesRent.discountPercentage === 100 ? 0 : seriesRent.discountPercentage
+      maxDiscountRate = Math.max(maxDiscountRate, seriesBuyDiscount, seriesRentDiscount)
+    } 
+    return maxDiscountRate
+  })();
+  const hasDiscount = discountRate > MIN_DISCOUNT_RATE
+  
+  if (isRentalBadgeEnableGenre && isRental) {
+    return { 
+      type: CircleBadgeType.Rental 
     }
-    if (rent && !isSetbook) {
-      rentalDiscountRate = rent.discountPercentage;
-    }
-
-    const maxDiscountRate = Math.max(discountRate, rentalDiscountRate)
-
-    return maxDiscountRate < 10 ? null : {
+  } else if (hasDiscount && freeBookCount < MAX_FREE_BOOK_COUNT_FOR_DISCOUNT_RENDERING) {
+    return {
       type: CircleBadgeType.Discount,
-      rate: maxDiscountRate,
+      rate: discountRate,
     }
-  } else if (isSeries) {
-    const { freeBookCount, unit } = dto.series.property
-
-    const { buy, rent } = dto.priceInfo
-    const { buy: seriesBuy, rent: seriesRent } = dto.series.priceInfo
-
-    const discountRate = Math.max(
-      buy.discountPercentage,
-      rent.discountPercentage,
-      seriesBuy.discountPercentage,
-      seriesRent.discountPercentage,
-    )
-
-    if (discountRate < 10 && freeBookCount === 0) {
-      return null
-    } else if (discountRate >= 10 && freeBookCount < 4) {
-      return {
-        type: CircleBadgeType.Discount,
-        rate: discountRate,
-      }
-    }
+  } else if (hasFreeBook) {
     return {
       type: CircleBadgeType.Freebook,
       count: freeBookCount,
-      unit,
+      unit: freeBookUnit,
     }
   }
 
