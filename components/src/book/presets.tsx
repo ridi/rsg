f793c43402137966259163ee1ef1301249bff03a
Heejae Jang <heejae@ridi.com>
2018-03-09 18:49:55 +0900

    Fix: BookPresets shadow variable

--- components/src/book/presets.tsx
+++ components/src/book/presets.tsx
@@ -11,68 +11,68 @@ type presetFn = (Root: RootComponents, options: PresetOptions) => JSX.Element;
 
 const portrait: presetFn = ({ Thumbnail, Metadata }, options) => (
   <>
-    <Thumbnail.wrapper key='Thumbnail.wrapper' thumbnailSize={options.thumbnailSize}>
-      <Thumbnail.coverImage key='Thumbnail.coverImage' />
-      <Thumbnail.circleBadge key='Thumbnail.circleBadge' />
-      <Thumbnail.hdBadge key='Thumbnail.hdBadge' />
-      <Thumbnail.setBooklet key='Thumbnail.setBooklet' />
+    <Thumbnail.wrapper key="Thumbnail.wrapper" thumbnailSize={options.thumbnailSize}>
+      <Thumbnail.coverImage key="Thumbnail.coverImage" />
+      <Thumbnail.circleBadge key="Thumbnail.circleBadge" />
+      <Thumbnail.hdBadge key="Thumbnail.hdBadge" />
+      <Thumbnail.setBooklet key="Thumbnail.setBooklet" />
     </Thumbnail.wrapper>
-    <Metadata.wrapper key='Metadata.wrapper' width={options.thumbnailSize}>
-      <Metadata.title key='Metadata.title' />
-      <Metadata.authors key='Metadata.authors' simple={true} />
-      <Metadata.starRate key='Metadata.starRate' />
-      <Metadata.price key='Metadata.price' />
-      <Metadata.bookTypeBadge key='Metadata.bookTypeBadge' />
-      <Metadata.someDealBadge key='Metadata.someDealBadge' />
+    <Metadata.wrapper key="Metadata.wrapper" width={options.thumbnailSize}>
+      <Metadata.title key="Metadata.title" />
+      <Metadata.authors key="Metadata.authors" simple={true} />
+      <Metadata.starRate key="Metadata.starRate" />
+      <Metadata.price key="Metadata.price" />
+      <Metadata.bookTypeBadge key="Metadata.bookTypeBadge" />
+      <Metadata.someDealBadge key="Metadata.someDealBadge" />
     </Metadata.wrapper>
   </>
 );
 
 const metadataLandscape: presetFn = ({ Thumbnail, Metadata }, options) => (
   <>
-    <Thumbnail.wrapper key='Thumbnail.wrapper' thumbnailSize={options.thumbnailSize}>
-      <Thumbnail.coverImage key='Thumbnail.coverImage' />
-      <Thumbnail.circleBadge key='Thumbnail.circleBadge' />
-      <Thumbnail.hdBadge key='Thumbnail.hdBadge' />
-      <Thumbnail.setBooklet key='Thumbnail.setBooklet' />
+    <Thumbnail.wrapper key="Thumbnail.wrapper" thumbnailSize={options.thumbnailSize}>
+      <Thumbnail.coverImage key="Thumbnail.coverImage" />
+      <Thumbnail.circleBadge key="Thumbnail.circleBadge" />
+      <Thumbnail.hdBadge key="Thumbnail.hdBadge" />
+      <Thumbnail.setBooklet key="Thumbnail.setBooklet" />
     </Thumbnail.wrapper>
-    <Metadata.wrapper key='Metadata.wrapper' layout='landscape'>
-      <Metadata.title key='Metadata.title' />
-      <Metadata.subTitle key='Metadata.subTitle' />
-      <div key='Metadata.info' className='RSGBookMetadata_Info'>
-        <Metadata.starRate key='Metadata.starRate' />
-        <Metadata.authors key='Metadata.authors' simple={true} />
-        <Metadata.count key='Metadata.count' />
-        <Metadata.publisher key='Metadata.publisher' />
+    <Metadata.wrapper key="Metadata.wrapper" layout="landscape">
+      <Metadata.title key="Metadata.title" />
+      <Metadata.subTitle key="Metadata.subTitle" />
+      <div key="Metadata.info" className="RSGBookMetadata_Info">
+        <Metadata.starRate key="Metadata.starRate" />
+        <Metadata.authors key="Metadata.authors" simple={true} />
+        <Metadata.count key="Metadata.count" />
+        <Metadata.publisher key="Metadata.publisher" />
       </div>
-      <Metadata.description key='Metadata.description' />
-      <Metadata.price key='Metadata.price' />
-      <Metadata.bookTypeBadge key='Metadata.bookTypeBadge' />
-      <Metadata.someDealBadge key='Metadata.someDealBadge' />
+      <Metadata.description key="Metadata.description" />
+      <Metadata.price key="Metadata.price" />
+      <Metadata.bookTypeBadge key="Metadata.bookTypeBadge" />
+      <Metadata.someDealBadge key="Metadata.someDealBadge" />
     </Metadata.wrapper>
   </>
 );
 
 const landscape: presetFn = ({ Thumbnail, Metadata }, options) => (
   <>
-    <Thumbnail.wrapper key='Thumbnail.wrapper' thumbnailSize={options.thumbnailSize}>
-      <Thumbnail.coverImage key='Thumbnail.coverImage' />
-      <Thumbnail.circleBadge key='Thumbnail.circleBadge' />
-      <Thumbnail.hdBadge key='Thumbnail.hdBadge' />
-      <Thumbnail.setBooklet key='Thumbnail.setBooklet' />
+    <Thumbnail.wrapper key="Thumbnail.wrapper" thumbnailSize={options.thumbnailSize}>
+      <Thumbnail.coverImage key="Thumbnail.coverImage" />
+      <Thumbnail.circleBadge key="Thumbnail.circleBadge" />
+      <Thumbnail.hdBadge key="Thumbnail.hdBadge" />
+      <Thumbnail.setBooklet key="Thumbnail.setBooklet" />
     </Thumbnail.wrapper>
-    <Metadata.wrapper key='Metadata.wrapper'>
-      <Metadata.title key='Metadata.title' />
-      <Metadata.subTitle key='Metadata.subTitle' />
-      <div key='Metadata.info' className='RSGBookMetadata_Info'>
-        <Metadata.authors key='Metadata.authors' simple={true} />
-        <Metadata.count key='Metadata.count' />
-        <Metadata.publisher key='Metadata.publisher' />
-        <Metadata.starRate key='Metadata.starRate' />
+    <Metadata.wrapper key="Metadata.wrapper">
+      <Metadata.title key="Metadata.title" />
+      <Metadata.subTitle key="Metadata.subTitle" />
+      <div key="Metadata.info" className="RSGBookMetadata_Info">
+        <Metadata.authors key="Metadata.authors" simple={true} />
+        <Metadata.count key="Metadata.count" />
+        <Metadata.publisher key="Metadata.publisher" />
+        <Metadata.starRate key="Metadata.starRate" />
       </div>
-      <Metadata.price key='Metadata.price' />
-      <Metadata.bookTypeBadge key='Metadata.bookTypeBadge' />
-      <Metadata.someDealBadge key='Metadata.someDealBadge' />
+      <Metadata.price key="Metadata.price" />
+      <Metadata.bookTypeBadge key="Metadata.bookTypeBadge" />
+      <Metadata.someDealBadge key="Metadata.someDealBadge" />
     </Metadata.wrapper>
   </>
 );
@@ -81,7 +81,7 @@ export interface PresetOptions {
   thumbnailSize: number;
 }
 
-function preset(fn: presetFn, props: Partial<ComponentProps>) {
+function preset(fn: presetFn, presetProps: Partial<ComponentProps>) {
   return (props: ComponentProps & PresetOptions) => {
     const {
       children,
@@ -94,7 +94,7 @@ function preset(fn: presetFn, props: Partial<ComponentProps>) {
     };
 
     return (
-      <Book {...componentProps} {...props}>
+      <Book {...componentProps} {...presetProps}>
         {(Root) => typeof children === 'function'
           ? children(Root, fn(Root, options))
           : fn(Root, options)
