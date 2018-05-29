8b8c935bfd7c755c0dcb85eee2574dd53d23a007
gyujincho <gyujincho@ridi.com>
2018-05-29 18:26:35 +0900

    Update rui popup component in rsg

--- components/src/popup/index.tsx
+++ components/src/popup/index.tsx
@@ -5,35 +5,42 @@ import { Link } from 'react-router-dom';
 export interface PopupProps {
   title: string;
   active: boolean;
-  activeTabIndex?: number;
   caution?: string;
-  onTabClick?: (index: number) => void;
-  onCancel: () => void;
-  onConfirm: () => void;
-  children?: string;
   tabs?: Array<{
     name: string;
     content: React.ReactElement<any>;
   }>;
+  activeTabIndex?: number;
+  onTabClick?: (index: number) => void;
+  useButtons?: boolean;
+  showFooterHr?: boolean;
+  cancelButtonName?: string;
+  onCancel: () => void;
+  confirmButtonName?: string;
+  onConfirm?: () => void;
 }
 
 const noop = (): any => null;
 
 export const Popup: React.SFC<PopupProps> = (props) => {
   const {
-    active,
     title,
+    active,
     children,
     caution,
     activeTabIndex = 0,
-    onTabClick = noop,
-    onCancel = noop,
-    onConfirm = noop,
     tabs = [],
+    onTabClick = noop,
+    useButtons = false,
+    showFooterHr = true,
+    cancelButtonName,
+    onCancel = noop,
+    confirmButtonName,
+    onConfirm = noop,
   } = props;
   const wrapperClass = classNames(['rui_popup', active && 'active']);
   return (
-    <div id="test_popup" className={wrapperClass}>
+    <div className={wrapperClass}>
       <div className="popup_dimmed_bg" onClick={onCancel} />
       <div className="popup_contents">
         <div className="popup_header">
@@ -57,19 +64,36 @@ export const Popup: React.SFC<PopupProps> = (props) => {
             ? tabs[activeTabIndex].content
             : <p className="popup_body_paragraph">{children}</p>
           }
-          {/* <p className="popup_body_paragraph">RUI 팝업내용입니다.</p> */}
         </div>
-        <div className="popup_footer last_block">
-          <hr className="popup_footer_hr" />
-          <ul className="popup_buttons_wrapper">
-            <li className="button_list">
-              <button type="button" className="button rui_button_white_40" onClick={onCancel}>취소</button>
-            </li>
-            <li className="button_list">
-              <button type="button" className="button rui_button_blue_40" onClick={onConfirm}>확인</button>
-            </li>
-          </ul>
-        </div>
+        {useButtons && (
+          <div className="popup_footer last_block">
+            {showFooterHr && <hr className="popup_footer_hr" />}
+            <ul className="popup_buttons_wrapper">
+              {cancelButtonName && (
+                <li className="button_list">
+                  <button
+                    type="button"
+                    className="button rui_button_white_40"
+                    onClick={onCancel}
+                  >
+                    {cancelButtonName}
+                  </button>
+                </li>
+              )}
+              {confirmButtonName && (
+                <li className="button_list">
+                  <button
+                    type="button"
+                    className="button rui_button_blue_40"
+                    onClick={onConfirm}
+                  >
+                    {confirmButtonName}
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
