74b060f765a564c2d49f6643b6b8b2ef73f77452
Hyeonsu Lee <incleaf@gmail.com>
2018-08-29 18:14:52 +0900

    Couple button stylesheet with its component

--- components/scripts/build.js
+++ components/scripts/build.js
@@ -3,21 +3,25 @@
 const async = require('async');
 const { rollup } = require('rollup');
 const generateOptions = require('./option');
+const cssBuilder = require('./cssBuilder');
 
 const modules = ['icon', 'book', 'pagination', 'order', 'popup', 'empty', 'fetch_retry_block', 'button', 'check_box'];
 module.exports = new Promise(resolve => {
-  async.series([...modules.map(moduleName => async callback => {
-    const options = generateOptions(moduleName);
-    try {
-      const bundle = await rollup(options.input);
-      await bundle.generate(options.output);
-      await bundle.write(options.output);
-      console.log(`- Build ${moduleName} component`);
-    } catch (error) {
-      console.log(error);
-    }
-    callback(null, '');
-  })], (err, result) => {
+  async.series([
+    cssBuilder,
+    ...modules.map(moduleName => async callback => {
+      const options = generateOptions(moduleName);
+      try {
+        const bundle = await rollup(options.input);
+        await bundle.generate(options.output);
+        await bundle.write(options.output);
+        console.log(`- Build ${moduleName} component`);
+      } catch (error) {
+        console.log(error);
+      }
+      callback(null, '');
+    }),
+  ], (err, result) => {
     resolve(result);
   });
 });
