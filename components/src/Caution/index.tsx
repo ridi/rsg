e2e1ed115ac87a2afdf7fb7b1cd505789d46fe12
nnyamm <nnyamm@ridi.com>
2018-10-01 18:41:19 +0900

    Add layout prop

--- components/src/Caution/index.tsx
+++ components/src/Caution/index.tsx
@@ -4,12 +4,17 @@ import classNames from 'classNames';
 import { ButtonProps } from 'dist/Button';
 import * as React from 'react';
 
+export enum CautionTheme {
+  Brown = 'brown',
+  Gray = 'gray',
+}
+
 export interface CautionProps {
   title?: string;
   description?: string;
   icon?: keyof IconsInterface;
-  theme?: 'brown' | 'gray';
-  isAlignCenter?: boolean;
+  theme?: CautionTheme;
+  isLayoutPortrait?: boolean;
   inlineLinkPath?: string;
   inlineLinkLabel?: string;
   button?: React.SFC<ButtonProps>;
@@ -20,48 +25,53 @@ export const Caution: React.SFC<CautionProps> = (props) => {
     title,
     description,
     icon,
-    theme = 'brown',
-    isAlignCenter,
+    theme = CautionTheme.Brown,
+    isLayoutPortrait = false,
     inlineLinkPath,
     inlineLinkLabel,
     button,
   } = props;
+
   return (
     <div className={classNames([
       'RUICaution',
       `RUICaution-color-${theme}`,
-      isAlignCenter && 'RUICaution-align-center',
+      `RUICaution-layout-${isLayoutPortrait ? 'portrait RUICaution-align-center' : 'vertical'}`,
     ])}>
-      {title &&
-        <h1 className="RUICaution_Title">
-          {icon && <Icon className="RUICaution_Icon" name={icon} />}
-          {title}
-        </h1>
+      <div className="RUICaution_Cell">
+        {title &&
+          <h1 className="RUICaution_Title">
+            {icon && <Icon className="RUICaution_Icon" name={icon} />}
+            {title}
+          </h1>
+        }
+        {description &&
+          <p className="RUICaution_Description">
+            {icon && !title && <Icon className="RUICaution_Icon" name={icon} />}
+            {description.split('\n').map((line, index) => (
+              <>
+                {index > 0 && <br />}
+                {line}
+              </>
+            ))}
+            {inlineLinkPath && inlineLinkLabel &&
+              <>
+                <br />
+                <a
+                  className="RUICaution_Description_InlineLink"
+                  href={inlineLinkPath}
+                >
+                  {inlineLinkLabel}
+                  <Icon className="RUICaution_Icon" name="arrow_9_right" />
+                </a>
+              </>
+            }
+          </p>
+        }
+      </div>
+      {button &&
+        <div className="RUICaution_Cell RUICaution_Button">{button}</div>
       }
-      {description &&
-        <p className="RUICaution_Description">
-          {icon && !title && <Icon className="RUICaution_Icon" name={icon} />}
-          {description.split('\n').map((line, index) => (
-            <>
-              {index > 0 && <br />}
-              {line}
-            </>
-          ))}
-          {inlineLinkPath && inlineLinkLabel &&
-            <>
-              <br />
-              <a
-                className="RUICaution_Description_InlineLink"
-                href={inlineLinkPath}
-              >
-                {inlineLinkLabel}
-                <Icon className="RUICaution_Icon" name="arrow_9_right" />
-              </a>
-            </>
-          }
-        </p>
-      }
-      {button}
     </div>
   );
 };
