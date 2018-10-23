fcb436b1fb684b35ce61c0148e456fe420a9d4c5
nnyamm <nnyamm@ridi.com>
2018-10-23 18:23:12 +0900

    Add Readme

--- components/scripts/config.js
+++ components/scripts/config.js
@@ -7,6 +7,7 @@ const tempDir = tmpdir();
 const modules = [
   'Icon',
   'book',
+  'LibraryBook',
   'Order',
   'Popup',
   'Empty',
---
+++ components/src/LibraryBook/README.md
@@ -0,0 +1,6 @@
+```js
+<LibraryBook.Thumbnail
+  thumbnailUrl="//misc.ridibooks.com/cover/2008010605/small"
+  title="test"
+/>
+```
