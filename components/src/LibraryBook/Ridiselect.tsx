e6e949de0af067efc084ee941d0f397396371808
nnyamm <nnyamm@ridi.com>
2018-11-29 19:24:20 +0900

    Modify Ridiselect Icon

--- components/src/LibraryBook/Ridiselect.tsx
+++ components/src/LibraryBook/Ridiselect.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export const Ridiselect: React.SFC = () => (
   <p className="Ridiselect">
-    <Icon className="Ridiselect_Icon" name="book_1" />
+    <Icon className="Ridiselect_Icon" name="logo_ridiselect_2" />
     리디셀렉트
   </p>
 );
